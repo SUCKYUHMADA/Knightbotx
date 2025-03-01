f13fd9f720ccd234aae1c22bcd625e33c10f709b
SUCKYUHMADA <suckyuhmada71@gmail.com>
2025-02-28 21:59:54 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'JASON', // Your name
+  ownerNumber: '18768169577', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
